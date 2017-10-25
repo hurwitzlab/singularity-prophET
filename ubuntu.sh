34a35abc15c2be6bc8b8593798bee2bc73e541e9
Scott G. Daniel <scottdaniel25@gmail.com>
2017-10-25 19:05:59 +0000

    it works

--- ubuntu.sh
+++ ubuntu.sh
@@ -13,8 +13,20 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     sed -i 's/$/ universe/' /etc/apt/sources.list
 
     #essential stuff
-    apt -y --force-yes install git sudo man vim build-essential wget unzip curl autoconf libtool pkg-config libgd2-xpm-dev subversion
- 
+    apt -y --force-yes install git sudo man vim build-essential wget unzip curl autoconf libtool pkg-config subversion
+#libgd2-xpm-dev 
+	#how the broad people do it
+	apt-get -y --force-yes update
+	apt-get -y --force-yes autoremove
+	apt-get -y --force-yes install libdb5.3++-dev
+	apt-get -y --force-yes install emboss
+	apt-get -y --force-yes install bedtools
+	apt-get -y --force-yes install libgd3
+	apt-get -y --force-yes install libgd-dev
+	apt-get -y --force-yes install libgd-perl  
+	apt-get -y --force-yes install libgd-svg-perl
+	apt-get -y --force-yes install bioperl 
+    
     mkdir /apps
     cd /apps
  
@@ -25,8 +37,8 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     sudo ln -s /apps/miniconda/bin/python2.7 /usr/bin/python
     PATH="/apps/miniconda/bin:$PATH"
     conda install -y -c bioconda blast-legacy
-    conda install -y -c bioconda emboss
-    conda install -y -c bioconda bedtools
+#    conda install -y -c bioconda emboss
+#    conda install -y -c bioconda bedtools
  
     #so we dont get those stupid perl warnings
     locale-gen en_US.UTF-8
@@ -35,13 +47,23 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     wget -O - http://cpanmin.us | perl - --self-upgrade
 
     #Perl-modules for prophet
-    cpanm Bio::Perl
-    cpanm SVG
-    cpanm GD
-    cpanm GD::SVG
-    cpanm Bio::Graphics
-    cpanm LWP::Simple
-    cpanm XML::Simple
+#    cpanm Bio::Perl
+#    cpanm SVG
+#    cpanm GD
+#    cpanm GD::SVG
+#    cpanm Bio::Graphics
+#    cpanm LWP::Simple
+#    cpanm XML::Simple
+#
+
+	#how broad people do it
+	cpanm SVG
+	cpanm GD
+	cpanm DB_File
+	cpanm --force GD::SVG
+	cpanm --force Bio::Graphics
+	cpanm LWP::Simple
+	cpanm XML::Simple
 
     git clone https://github.com/jaumlrc/ProphET.git prophet
     cd prophet
@@ -56,5 +78,5 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     mkdir /extra
     mkdir /xdisk
 
-%test
-    cd /apps/prophet && ./ProphET_standalone.pl --fasta test.fasta --gff_in test.gff --outdir test
+#%test
+#    cd /apps/prophet && ./ProphET_standalone.pl --fasta test.fasta --gff_in test.gff --outdir test
